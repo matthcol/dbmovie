953bd9ed299f2c4040314beb388c72dfb0d77393
matthcol <matthias.colin@gmail.com>
2021-04-22 08:24:51 +0200

    remove unused tables

--- pg_cine_ddl.sql
+++ pg_cine_ddl.sql
@@ -29,20 +29,6 @@ create table play(
 	constraint pk_play primary key(id_movie, id_actor)
 );
 
-create table franchise
-(
-    id serial constraint pk_franchise primary key,
-    name varchar(50) not null unique,
-    id_parent_franchise int null
-);
-
-create table in_franchise
-(
-    id_movie int,
-	id_franchise int,
-    CONSTRAINT pk_in_franchise PRIMARY KEY (id_movie, id_franchise)
-);
-
 alter table movies add constraint fk_movies_director FOREIGN KEY (id_director)
 references stars(id);
 alter table movies add constraint CHK_MOVIES_YEAR CHECK (year >= 1888);
@@ -53,8 +39,4 @@ references movies(id);
 alter table play add constraint FK_PLAY_ACTOR FOREIGN KEY (id_actor)
 references stars(id);
 
-alter table in_franchise add constraint fk_in_franchise_2 FOREIGN KEY (id_movie)
-references movies (id);
-alter table in_franchise add constraint fk_in_franchise_1 FOREIGN KEY (id_franchise)
-references franchise (id);
 
