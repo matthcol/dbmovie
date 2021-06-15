b279816e58e63a0640364c47b371f46cecbe8a80
matthcol <matthias.colin@gmail.com>
2021-06-15 08:12:36 +0200

    add table stars

---
+++ my_cine_ddl02.sql
@@ -0,0 +1,6 @@
+create table stars (
+	id integer auto_increment,
+	name varchar(150) not null,
+	birthdate date null,
+	constraint pk_star primary key(id)
+);
---
+++ pg_cine_ddl02.sql
@@ -0,0 +1,6 @@
+create table stars (
+	id serial constraint pk_stars primary key,
+	name varchar(150) not null,
+	birthdate date null,
+	deathdate date null
+);
