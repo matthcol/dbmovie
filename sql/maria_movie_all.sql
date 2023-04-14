8368bb411e984e1a5b303c607f181a056f9abd3d
matthcol <matthias.colin@gmail.com>
2023-04-14 08:59:24 +0200

    summary script

---
+++ sql/maria_movie_all.sql
@@ -0,0 +1,3 @@
+source 01-ddl.sql
+source 02-data_persons.sql
+source 03-data_movies.sql
--- sql/pg_movie_all.sql
+++
@@ -1,8 +0,0 @@
-SET CLIENT_ENCODING = 'UTF-8';
-
-\i 01-pg-dbmovie-ddl.sql
-\i 02-persons.sql
-\i 03-movies.sql
-\i 04-play.sql
-\i 05-genres.sql
-\i 06-realign-seq.sql
