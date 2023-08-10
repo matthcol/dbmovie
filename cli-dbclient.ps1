47eacfef97985c4c60e012172c6cf7f21d54d3f5
matthcol <matthias.colin@gmail.com>
2023-08-10 13:30:51 +0200

    docker mysql

---
+++ cli-dbclient.ps1
@@ -0,0 +1,14 @@
+# create a DB container with a GUI client
+# Usage: cli-dbclient.ps1 [composition-name [env-file]]
+
+# Args:
+$composition_name="mysql-dbmovie"
+$env_file=".env"
+if ($args.Count -ge 1) {
+    $composition_name=$args[0]
+}
+if ($args.Count -ge 2) {
+    $env_file=$args[1]
+}
+Write-Debug "Start DB client for composition [$composition_name] with env file [$env_file]"
+docker compose -p "$composition_name" exec -it db mysql -u movie -ppassword dbmovie
