2fac769c37c69daa9c6373f47945077b9bc7a6c4
matthcol <matthias.colin@gmail.com>
2023-10-28 11:03:35 +0200

    adapt data for tsql

--- sql/01-tables.sql
+++ sql/01-tables.sql
@@ -1,12 +1,12 @@
-create table persons (
-	id integer auto_increment,
+create table person (
+	id integer identity,
 	name varchar(150) not null,
 	birthdate date null,
-	constraint pk_persons primary key(id)
+	constraint pk_person primary key(id)
 );
 
-create table movies (
-	id integer auto_increment,
+create table movie (
+	id integer identity,
 	title varchar(300) not null,
 	year smallint not null,
 	duration smallint null,
@@ -15,9 +15,9 @@ create table movies (
 	color varchar(20) null,
 	pg varchar(15) null,
 	director_id integer null,
-	constraint pk_movies primary key(id),
-	constraint uniq_movies UNIQUE(title, year),
-	constraint chk_movies_year CHECK(year >= 1850)
+	constraint pk_movie primary key(id),
+	constraint uniq_movie UNIQUE(title, year),
+	constraint chk_movie_year CHECK(year >= 1850)
 );
 
 create table play(
@@ -32,17 +32,17 @@ create table have_genre(
 	genre varchar(20) not null
 );
 
-alter table movies add constraint FK_MOVIES_DIRECTOR 
+alter table movie add constraint FK_MOVIE_DIRECTOR 
 	FOREIGN KEY (director_id)
-	references persons(id);
+	references person(id);
 alter table play add constraint FK_PLAY_MOVIE 
 	FOREIGN KEY (movie_id)
-	references movies(id);
+	references movie(id);
 alter table play add constraint FK_PLAY_ACTOR 
 	FOREIGN KEY (actor_id)
-	references persons(id);
+	references person(id);
 alter table have_genre add constraint FK_HAVE_GENRE 
 	FOREIGN KEY (movie_id)
-	references movies(id);
+	references movie(id);
 
 
